b77ca629450ec827c58ce65c26315f74b1bda364
Olesia Zaidman <123760026+olesiaZaidman@users.noreply.github.com>
2023-07-26 23:09:49 +0100

    Initial Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 500f; // Adjust this value to control the player's movementToClick speed
+    Rigidbody rb;
+    // float moveHorizontal;
+    Vector3 targetPosition;
+    Vector3 mousePositionScreen;
+    Vector3 movementToClick;
+    Camera mainCamera;
+
+    bool selected = false;
+    Color defaultColor = Color.yellow;
+    Color colorSelected = Color.green;
+    Renderer rend;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+        rend = GetComponent<Renderer>();
+        SetGameObjectColor(defaultColor);
+    }
+
+    void Update()
+    {
+        HandleMouseClickInput();
+    }
+    public void SetGameObjectColor(Color color)
+    {
+        //Renderer renderer = gameObject.GetComponent<Renderer>();
+        if (rend != null)
+        {
+            rend.material.color = color;
+        }
+    }
+
+    void HandleMouseClickInput()
+    {
+        // Check for mouse click
+        if (Input.GetMouseButton(1) && selected)
+        {
+            // Get the mouse position in screen space
+            mousePositionScreen = Input.mousePosition;
+
+            // Convert the mouse position to world space
+            targetPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //new Vector3(mousePositionScreen.x, mousePositionScreen.y, transform.position.z)
+            targetPosition.z = 0;
+        }
+
+        if (Input.GetMouseButton(0) && selected)
+        {
+            SetGameObjectColor(defaultColor);
+            selected = false;
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        selected = true;
+        SetGameObjectColor(colorSelected);
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacterToMouseClick();
+    }
+
+    void MoveCharacterToMouseClick()
+    {
+        // Calculate the movementToClick vector towards the target position
+        //   movementToClick = new Vector3((targetPosition.x - transform.position.x) * 
+        //     moveSpeed * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+
+        // Apply the movementToClick to the Rigidbody
+        //  rb.velocity = movementToClick;
+        float speed = moveSpeed * Time.fixedDeltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed);
+
+    }
+
+
+
+
+    //void HandleArrowsInput()
+    //{
+    //      moveHorizontal = Input.GetAxis("Horizontal");
+    //}
+
+    //void MoveCharacter()
+    //{
+    //    Vector3 movementToClick = new Vector3(moveHorizontal * moveSpeed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+    //    rb.velocity = movementToClick;
+    //}
+
+}
