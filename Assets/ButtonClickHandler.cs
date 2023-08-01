e5df6400db6072fcb49a2949f455833056bb1d3a
Olesia Zaidman <123760026+olesiaZaidman@users.noreply.github.com>
2023-08-01 17:22:36 +0100

    ButtonClickHandler
    testing Event Handlers

--- Assets/ButtonClickHandler.cs
+++ Assets/ButtonClickHandler.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ButtonClickHandler : MonoBehaviour
+public class ButtonClickHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
+    IPointerDownHandler, IPointerClickHandler   //IPointerUpHandler? //using UnityEngine.EventSystems;
 {
     ImageFillOverTime imageFillOverTime;
     [SerializeField] Button myButton;
+
+    public class CustomUIEvent : UnityEvent { } //using UnityEngine.Events;
+    public CustomUIEvent OnUiEvent;
     void Start()
     {
         imageFillOverTime = GetComponent<ImageFillOverTime>();
+
+        if (imageFillOverTime == null)
+        {
+            imageFillOverTime = GetComponentInParent<ImageFillOverTime>();  
+        }
+
+        myButton.onClick.AddListener(OnButtonClick); 
     }
 
-    public void ResetButtonColor()
+    void OnButtonClick()
     {
-        StartCoroutine(ResetButtonColorCoroutine());
+        imageFillOverTime.StartImageFillOverTime();
+        DeselectButtonOnFillComplete();
     }
 
-    private IEnumerator ResetButtonColorCoroutine()
+
+    public void DeselectButtonOnFillComplete() //DeselectButtonOnFillComplete
+    {
+        StartCoroutine(DeselectButtonCoroutine());
+    }
+
+    private IEnumerator DeselectButtonCoroutine() //DeselectButtonCoroutine
     {
         // Wait until the image fill is complete
         yield return new WaitUntil(() => imageFillOverTime.IsFillComplete);
@@ -37,4 +56,24 @@ public class ButtonClickHandler : MonoBehaviour
     {
         myButton.interactable = true;
     }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Entered");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exited");
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked - works only when script is attached to the Button");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Down - works only when script is attached to the Button");
+    }
+
+  
 }
